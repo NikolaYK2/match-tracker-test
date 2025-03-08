fbf8f2c67a9166de339c04114112b056fd2830a9
k2 <izmail.k2@gmail.com>
2025-03-08 18:37:11 +0300

    chore: add tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: {
+          DEFAULT: "#0f0f0f",
+          bg: "#1a1a1a",
+          card: "#222222",
+          border: "#2c2c2c",
+        },
+        accent: {
+          primary: "#e9355a",
+          secondary: "#6c4ef6",
+          green: "#22c55e",
+          orange: "#f97316",
+          red: "#ef4444",
+          yellow: "#facc15",
+        },
+        light: {
+          DEFAULT: "#ffffff",
+          gray: "#B4B5B6",
+          dark: "#FAFAFA66",
+        },
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
