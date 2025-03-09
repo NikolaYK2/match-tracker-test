69038f6c1549c18b56f68070298600e22a73a39e
k2 <izmail.k2@gmail.com>
2025-03-09 23:02:35 +0300

    chore: add export svg

--- app/_common/components/icons/indexIcons.ts
+++ app/_common/components/icons/indexIcons.ts
@@ -1,3 +1,4 @@
 export { default as Item } from "./Item";
 export { default as Union } from "./Union";
 export { default as Vector } from "./Vector";
+export { default as Chevron } from "./Chevron";
