0aae55644f9eb9e5e53b52c5b3e17174d34cd1b7
k2 <izmail.k2@gmail.com>
2025-03-09 13:49:45 +0300

    fix: import

--- app/_common/utils/customFonts.ts
+++ app/_common/utils/customFonts.ts
@@ -1,7 +1,7 @@
 import localFont from "next/font/local";
 
 export const tacticSans = localFont({
-  src: "@/app/fonts/tacticSans/tactic-sans-regular.ttf",
+  src: "../../fonts/tacticSans/tactic-sans-regular.ttf",
   variable: "--font-tacticSans",
   display: "swap",
   weight: "400",
