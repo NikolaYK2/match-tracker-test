dcbd6a51cb213a1edd264372deb85285fba6c7fc
k2 <izmail.k2@gmail.com>
2025-03-10 12:00:05 +0300

    style: add variant

--- app/_common/components/button/Button.tsx
+++ app/_common/components/button/Button.tsx
@@ -2,7 +2,7 @@ import { ComponentPropsWithoutRef } from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cn } from "@/app/_common/utils";
 
-type Variant = "primary";
+type Variant = "primary" | "link";
 type Props = {
   variant?: Variant;
   fullWidth?: boolean;
@@ -11,5 +11,10 @@ type Props = {
 export const Button = ({ variant = "primary", fullWidth = false, asChild, className, ...props }: Props) => {
   const Comp = asChild ? Slot : "button";
 
-  return <Comp {...props} className={cn("text-light px-[40px] py-5", variant, fullWidth && "w-full", className)} />;
+  return (
+    <Comp
+      {...props}
+      className={cn("text-light px-[40px] py-5 disabled:opacity-[0.4]", variant, fullWidth && "w-full", className)}
+    />
+  );
 };
